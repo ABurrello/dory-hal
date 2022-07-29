5583129556106dca313d736313ba4f2f179baf6a
ABurrello <burry94>
2022-07-29 14:42:16 +0200

    added __init__.py files to create dory library

--- src/analog_conv_2d.c
+++ src/analog_conv_2d.c
@@ -23,12 +23,11 @@ int32_t analog_conv_2d(const void *input_L2,
     analog_encoder_instruction_memory((uint32_t) input, (uint32_t) weights, (uint32_t) output, layer);
     hwme_ana_im_addr_set(instruction_memory_compiled_analog);
     hwme_ana_im_n_set(32);
-    hwme_ana_act_addr_set((uint32_t) input_L2); 
     //hwme_ana_act_n_set(( layer->c * layer->cx * layer->cy ) / 4); 
-    hwme_ana_act_n_set(0); 
     hwme_ana_wt_addr_set((uint32_t) weights_L2);
-    ///// TO SET
-    hwme_ana_simd_addr_set((uint32_t) batchnorm_L2);
+    hwme_ana_simd_addr_set((uint32_t) batchnorm_L2); // TO SET
+    hwme_ana_act_n_set(0); 
+    hwme_ana_nb_tile_set(0);
     // STEP 3: triggering of the operations
     // The pointer to the instruction memory.
     hwme_ana_trigger_job();
